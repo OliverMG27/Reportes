cde878602c140334fdbc215c06ae1a7078f584ae
OliverMG27 <oliver27jean@gmail.com>
2023-10-16 12:36:35 -0600

    Agregar archivos de proyecto.

---
+++ Pages/Shared/_NewLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Reportes.Pages.Shared
+{
+    public class _NewLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
