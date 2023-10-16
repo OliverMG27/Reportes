773ff4560b4c94eb20ee4c38981d4f0545075a39
OliverMG27 <oliver27jean@gmail.com>
2023-10-16 14:21:56 -0600

    Primer commit con menu agregado

---
+++ Pages/Menu/MenuI.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Reportes.Pages.Menu
+{
+    public class MenuIModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
