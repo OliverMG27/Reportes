e3bb5e04f7da0c48c1e697d34422f1f41b6c083a
OliverMG27 <oliver27jean@gmail.com>
2023-10-28 08:55:17 -0600

    componente pruebas

---
+++ Pages/Pruebas/PruebasI.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Reportes.Pages.Pruebas
+{
+    public class PruebasIModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
