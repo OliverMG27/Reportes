c274f5f25e188531efcdf48b75a05b837b31189b
Fabian Sanchez Hernandez <78711059+FabianSanchez3@users.noreply.github.com>
2023-11-12 15:01:31 -0600

    models de tablas de empresa, granja

---
+++ Models/Empresa.cs
@@ -0,0 +1,8 @@
+﻿namespace Reportes.Models
+{
+    public class Empresa
+    {
+        public int id { get; set; }
+        public string? RazonSocial { get; set; }
+    }
+}
---
+++ Models/Granja.cs
@@ -0,0 +1,8 @@
+﻿namespace Reportes.Models
+{
+    public class Granja
+    {
+        public int idEmpresa { get; set; }
+        public string? granja { get; set; }
+    }
+}
