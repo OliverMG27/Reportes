8fc580667728c70f1f2cfc7dec86c5de5f2a7da8
Fabian Sanchez Hernandez <78711059+FabianSanchez3@users.noreply.github.com>
2023-11-21 10:44:52 -0600

    reportes mortalidad

--- Models/DetalleCerdo.cs
+++ Models/DetalleCerdo.cs
@@ -5,7 +5,7 @@
         public int idEmpresa { get; set; }
         public string? razonSocial { get; set; }
         public string? granja { get; set; }
-        public string? fechaMovimiento { get; set; }
+        public DateTime fechaMovimiento { get; set; }
         public string? lote {  get; set; }
         public string? nave {  get; set; }
         public int cantidad { get; set; }
